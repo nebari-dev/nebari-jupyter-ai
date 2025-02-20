9eb0064e08253eb6a141d1702bee917b8272a5e9
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-02-20 14:43:11 -0600

    working

--- nebari_jupyter_ai/_static_handler.py
+++ nebari_jupyter_ai/_static_handler.py
@@ -1,3 +1,4 @@
+from jupyter_server.utils import url_path_join
 from tornado.web import StaticFileHandler
 import os
 
@@ -7,19 +8,21 @@ NEBARI_ASSISTANT_AVATAR_ICON_PATH = str(os.path.join(os.path.dirname(__file__), 
 
 def setup_handlers(web_app):
     host_pattern = ".*$"    
-    
+        
+    # Get the base_url from the web_app  settings
+    base_url = web_app.settings.get('base_url', '/')
+    print('=' * 80)
+    print(f"base_url: {base_url}")
+    print('=' * 80)
+
     handlers = [(
-        rf"{NEBARI_ASSISTANT_PERSONA.avatar_route}()", # the `()` at the end of the URL denotes an empty regex capture group
+        rf"{url_path_join(base_url, NEBARI_ASSISTANT_PERSONA.avatar_route)}()", # the `()` at the end of the URL denotes an empty regex capture group
         StaticFileHandler, 
         {"path": NEBARI_ASSISTANT_AVATAR_ICON_PATH}
     )]
-    print('='*50)
-    print(handlers)
-    print('='*50)
     web_app.add_handlers(host_pattern, handlers)
 
-    
+
 if __name__ == "__main__":
-        base_path = os.path.dirname(__file__)
-        print(os.path.join(base_path, "static"))
-        
+    base_path = os.path.dirname(__file__)
+    print(os.path.join(base_path, "static"))
