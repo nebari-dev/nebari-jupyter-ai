9ffde42507e109da91daa58ad8c1ac5cc2aa1e7d
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-02-20 09:26:52 -0600

    fix typo

--- nebari_jupyter_ai/__init__.py
+++ nebari_jupyter_ai/__init__.py
@@ -7,7 +7,7 @@ def _jupyter_server_extension_points():
         "module": "nebari_jupyter_ai"
     }]
 
-def load_jupyter_server_extension(nb_server_app: jupyter_server.serverapp.ServerApp):
+def _load_jupyter_server_extension(nb_server_app: jupyter_server.serverapp.ServerApp):
     # This function is called when the extension is loaded.
     setup_handlers(nb_server_app.web_app)
 
